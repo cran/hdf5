4909f99aa136f48fb4d976f16d63fab5a384cef0
Marcus G. Daniels <mgd@santafe.edu>
2006-02-22 00:00:00 +0000

    version 1.6.2

--- R/hdf5.R
+++ R/hdf5.R
@@ -2,18 +2,20 @@
 {
   call <- sys.call()
   .External("do_hdf5load", call, sys.frame(sys.parent()), file, load,
-            as.integer (verbosity), as.logical(tidy))
+            as.integer (verbosity), as.logical(tidy),
+            PACKAGE="hdf5")
 }   
 
 "hdf5save" <- function (fileout, ...)
 {
   call <- sys.call()
   invisible(.External("do_hdf5save", call, sys.frame(sys.parent()),
-                      fileout, ...))
+                      fileout, ..., PACKAGE="hdf5"))
 }
 
 "hdf5cleanup" <- function (fid)
 {
   call <- sys.call()
-  invisible(.External("do_hdf5cleanup", call, sys.frame(sys.parent()), fid))
+  invisible(.External("do_hdf5cleanup", call, sys.frame(sys.parent()), fid,
+            PACKAGE="hdf5"))
 }
