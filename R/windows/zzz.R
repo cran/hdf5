5fa879c86f8cbc67de9788f87e5d7c0b21fe4c10
Marcus G. Daniels <mdaniels@lanl.gov>
2007-04-14 00:00:00 +0000

    version 1.6.6

--- R/windows/zzz.R
+++ R/windows/zzz.R
@@ -4,6 +4,7 @@
 {
   opath <-  Sys.getenv("PATH")
   libbin <- file.path(R.home(), "library/hdf5/libs")
+  if(!exists("Sys.setenv")) Sys.setenv <- Sys.putenv
   Sys.setenv(PATH=paste(libbin, opath, sep=";"))
   library.dynam("hdf5", pkg, lib)
   Sys.setenv(PATH=opath)
